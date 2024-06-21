a2077d8117b6e32c34e4f1f76e9f7423a1104d7c
Renan Saccon <sacconrenan@gmail.com>
2024-06-20 23:02:24 -0300

    Initial commit

---
+++ app/src/main/java/saccon/renan/br/trocatelas/MainActivity.kt
@@ -0,0 +1,26 @@
+package saccon.renan.br.trocatelas
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btLancamento : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btLancamento = findViewById(R.id.btLancamento) //Link do Botão Front com o End. Criando o objeto btLancamento
+
+        btLancamento.setOnClickListener{
+            btLancamentoOnClick()
+        }
+
+    }
+
+    private fun btLancamentoOnClick() {
+
+    }
+}
